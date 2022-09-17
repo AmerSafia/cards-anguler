469fc30fd27379e3f59fdb219e13661fbd7a4063
Amersafia <amer.safia321@gmail.com>
2022-09-17 17:13:17 +0300

    create cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'card 1',
+      image: 'assets/img/tree.jpeg',
+      username: 'nature',
+      contant: 'I saw this neat tree today',
+    },
+    {
+      title: 'card 2',
+      image: 'assets/img/mountain.jpeg',
+      username: 'miuntain',
+      contant: 'I saw this miuntain',
+    },
+    {
+      title: 'card 3',
+      image: 'assets/img/biking.jpeg',
+      username: 'biking',
+      contant: 'I saw this biking',
+    },
+    {
+      title: 'card 3',
+      image: 'assets/img/biking.jpeg',
+      username: 'biking',
+      contant: 'I saw this biking',
+    },
+  ];
 }
